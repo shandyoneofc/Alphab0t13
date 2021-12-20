0e1cb987afa0eb7232d14d399fbaea15dce702c9
shandyoneofc <95661048+shandyoneofc@users.noreply.github.com>
2021-12-20 23:03:59 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,27 +1,16 @@
 const donasibot = () => {
-return `DONASI BISA VIA BANK / E-MONEY
+return `DONASI BISA VIA E-MONEY
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 08** **** ****
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0817 7929 8306
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0817 7929 8306
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama R***** JB
 `
 	}
 
